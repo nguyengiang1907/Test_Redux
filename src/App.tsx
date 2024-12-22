e4aea1d423b3cc75bffd67e71edb148608059f86
nguyengiang1907 <ng19072004@gmail.com>
2024-12-22 22:21:21 +0700

    Update code

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-// File: App.tsx
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useDispatch } from 'react-redux';
-import BreedList from '../src/components/BreedList'; 
+import BreedList from '../src/components/BreedList';
 import { setData } from '../src/features/breedsSlice';
 
 const App: React.FC = () => {
+    const [isOnline, setIsOnline] = useState(navigator.onLine);
+    const [showOnlineMessage, setShowOnlineMessage] = useState(false);
+    const dispatch = useDispatch();
+
 
     const CallApi = async () => {
         try {
-            const reponse = (await axios.get('https://dogapi.dog/api/v2/breeds')).data;
+            if (navigator.onLine) {
+                const reponse = (await axios.get('https://dogapi.dog/api/v2/breeds')).data;
                 const transformedData = reponse.data.map((breed: any) => ({
                     id: breed.id,
                     name: breed.attributes.name,
@@ -22,21 +26,62 @@ const App: React.FC = () => {
                     type: breed.type,
                 }));
                 dispatch(setData(transformedData));
+
+                localStorage.setItem('breeds', JSON.stringify(transformedData))
+            } else {
+                const cachedData = localStorage.getItem('breeds');
+                if (cachedData) {
+                    const parsedData = JSON.parse(cachedData);
+                    dispatch(setData(parsedData));
+                } else {
+                    console.log('No cached data available.');
+                }
+            }
         } catch (error) {
             console.log(error);
         }
     }
 
-    const dispatch = useDispatch();  
     useEffect(() => {
-      CallApi()
-    }, [])
+        CallApi();
 
-  return (
-    <div>
-      <BreedList />
-    </div>
-  );
+        const handleOnline = () => {
+            setIsOnline(true);
+            setShowOnlineMessage(true);
+
+            setTimeout(() => {
+                setShowOnlineMessage(false);
+            }, 5000);
+        };
+
+        const handleOffline = () => {
+            setIsOnline(false);
+        };
+
+        window.addEventListener('online', handleOnline);
+        window.addEventListener('offline', handleOffline);
+
+        return () => {
+            window.removeEventListener('online', handleOnline);
+            window.removeEventListener('offline', handleOffline);
+        };
+    }, []);
+
+    return (
+        <div>
+            {!isOnline && (
+                <div className="alert alert-warning" role="alert">
+                    You are currently offline. Please check your connection.
+                </div>
+            )}
+            {showOnlineMessage && (
+                <div className="alert alert-success" role="alert">
+                    You are back online!
+                </div>
+            )}
+            <BreedList />
+        </div>
+    );
 };
 
 export default App;
--- src/components/BreedList.tsx
+++ src/components/BreedList.tsx
@@ -1,17 +1,35 @@
-// File: BreedList.tsx
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const BreedList: React.FC = () => {
-  const breeds = useSelector((state:any) => state.breeds.breeds);
-  console.log(breeds);
-  
+  const breeds = useSelector((state: any) => state.breeds.breeds);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const recordsPerPage = 5;
+
+  const totalPages = Math.ceil(breeds.length / recordsPerPage);
+  const startIndex = (currentPage - 1) * recordsPerPage;
+  const endIndex = startIndex + recordsPerPage;
+  const currentRecords = breeds.slice(startIndex, endIndex);
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) setCurrentPage(currentPage - 1);
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) setCurrentPage(currentPage + 1);
+  };
 
   return (
-    <div>
-      <table className="table table-bordered border-primary">
+    <div style={{ width: '90%', height: '100%' }}>
+      <table
+        className="table table-bordered border-primary"
+        style={{ marginTop: '5rem', marginLeft: '5%', marginRight: '5%' }}
+      >
         <thead>
           <tr>
+            <th>Id</th>
             <th>Name</th>
             <th>Life Expectancy</th>
             <th>Male Weight</th>
@@ -20,16 +38,46 @@ const BreedList: React.FC = () => {
           </tr>
         </thead>
         <tbody>
-          {breeds.map((breed: any) => (
+          {currentRecords.map((breed: any) => (
             <tr key={breed.id}>
+              <td>{breed.id}</td>
               <td>{breed.name}</td>
-              <td>{breed.life_min} - {breed.life_max} years</td>
-              <td>{breed.male_weight_min} - {breed.male_weight_max} kg</td>
-              <td>{breed.female_weight_min} - {breed.female_weight_max} kg</td>
+              <td>
+                {breed.life_min} - {breed.life_max} years
+              </td>
+              <td>
+                {breed.male_weight_min} - {breed.male_weight_max} kg
+              </td>
+              <td>
+                {breed.female_weight_min} - {breed.female_weight_max} kg
+              </td>
               <td>{breed.type}</td>
             </tr>
           ))}
         </tbody>
+        <tfoot>
+          <tr>
+            <td colSpan={6} className="text-center">
+              <button
+                className="btn btn-outline-primary mx-1"
+                onClick={handlePrevPage}
+                disabled={currentPage === 1}
+              >
+                &lt;
+              </button>
+              <span className="mx-2">
+                Page {currentPage} of {totalPages}
+              </span>
+              <button
+                className="btn btn-outline-primary mx-1"
+                onClick={handleNextPage}
+                disabled={currentPage === totalPages}
+              >
+                &gt;
+              </button>
+            </td>
+          </tr>
+        </tfoot>
       </table>
     </div>
   );
